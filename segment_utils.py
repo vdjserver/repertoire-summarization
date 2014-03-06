4f6393682262d2028c5a5f744bdaa74ece39d7ed
Edward A. Salinas <edward.salinas@utsouthwestern.edu>
2014-03-06 10:40:10 -0600

    import makeAllMapValuesVal

--- segment_utils.py
+++ segment_utils.py
@@ -10,7 +10,7 @@ from imgt_utils import get_loci_list,imgt_db
 import glob
 from utils import biopythonTranslate
 from igblast_utils import printNiceAlignment
-from utils import printMap
+from utils import printMap,makeAllMapValuesVal
 
 
 def removeTerminatingSemicolonIfItExists(s):
