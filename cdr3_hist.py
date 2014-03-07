74920751bad86a4f9cf7712294d69ff8da8039c4
Edward A. Salinas <edward.salinas@utsouthwestern.edu>
2014-03-07 13:41:17 -0600

    add lean left for CDR3 end

--- cdr3_hist.py
+++ cdr3_hist.py
@@ -345,7 +345,7 @@ def CDR3LengthAnalysis(vMap,jMap,organism,imgtdb_obj):
 				js_f=int(jMap['s. start'])
 				js_t=int(jMap['s. end'])
 				qry_cdr3_start=getQueryIndexGivenSubjectIndexAndAlignment(vq_aln,vs_aln,vq_f,vq_t,vs_f,vs_t,ref_cdr3_start)
-				qry_cdr3_end=getQueryIndexGivenSubjectIndexAndAlignment(jq_aln,js_aln,jq_f,jq_t,js_f,js_t,ref_cdr3_end)
+				qry_cdr3_end=getQueryIndexGivenSubjectIndexAndAlignment(jq_aln,js_aln,jq_f,jq_t,js_f,js_t,ref_cdr3_end,"left")
 				if(qry_cdr3_start!=(-1) and qry_cdr3_end!=(-1)):
 					#query_coding_seq=query_seq_map[currentQueryName]
 					#coding_seq=query_coding_seq[(qry_cdr3_start-1):(qry_cdr3_end-1)]
