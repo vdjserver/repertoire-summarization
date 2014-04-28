e8433c279b42213cfc4d00aae77ba464e7ad08f5
Edward A. Salinas <edward.salinas@utsouthwestern.edu>
2014-04-28 07:58:02 -0500

    script to delete intermediate/output files in test area

---
+++ Test_Data/del_intermediates.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#GET DIR http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+#echo $DIR
+rm -v $DIR/*.vdjml
+rm -v $DIR/*.out
+rm -v $DIR/*.txt
+rm -v $DIR/*.err
+rm -v $DIR/*.json
+rm -v $DIR/*.tsv
+rm -v $DIR/*.log
+rm -v $DIR/*.sim.fna
