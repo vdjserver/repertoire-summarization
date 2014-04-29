15b41ea7de8a350cdac1b9fa0575ec496be7d0b3
Edward A. Salinas <edward.salinas@utsouthwestern.edu>
2014-04-29 10:07:59 -0500

    improve comments remove extra echo

--- merge_tables.sh
+++ merge_tables.sh
@@ -2,16 +2,14 @@
 LOOP_NUM=1
 while [ -n "$1" ]
 	do
-	echo "Current Parameter: $1 , Remaining $#"
-	echo "LOOP_NUM $LOOP_NUM"
+	#echo "Current Parameter: $1 , Remaining $#"
+	#echo "LOOP_NUM $LOOP_NUM"
 	INFILE=$1
 	if [ $LOOP_NUM == "1" ] ; then
-		#echo "first loop"
-		#echo "INFILE is $INFILE"
+		#just cat the file!
 		cat $INFILE
 	else
-		#echo "subsequent loop...."
-		#echo "INFILE is $INFILE"
+		#cat the file BUT use awk to ignore the first line (NR==1)
 		cat $INFILE |awk '{if(NR>1) {print $0}}'
 	fi ;
 	#Pass $1 to some bash function or do whatever
