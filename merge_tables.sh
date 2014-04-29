f73f2228dbdbcbb281981e55cdc10941b32c645a
Edward A. Salinas <edward.salinas@utsouthwestern.edu>
2014-04-29 14:29:12 -0500

    work

--- merge_tables.sh
+++ merge_tables.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 LOOP_NUM=1
+LINES_ACCUM=0
 while [ -n "$1" ]
 	do
 	#echo "Current Parameter: $1 , Remaining $#"
@@ -8,6 +9,8 @@ while [ -n "$1" ]
 	if [ $LOOP_NUM == "1" ] ; then
 		#just cat the file!
 		cat $INFILE
+		LINES_ACCUM=`wc -l $INFILE`
+		LINES_ACCUM=`expr $LINES_ACCUM - 1`
 	else
 		#cat the file BUT use awk to ignore the first line (NR==1)
 		cat $INFILE |awk '{if(NR>1) {print $0}}'
