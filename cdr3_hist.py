c5f448ef2ae31d5363813d4b6c340a8e3880949b
Edward A. Salinas <edward.salinas@utsouthwestern.edu>
2014-06-05 14:20:18 -0500

    remove/comment debug/print statements

--- cdr3_hist.py
+++ cdr3_hist.py
@@ -434,10 +434,6 @@ def CDR3LengthAnalysis(vMap,jMap,organism,imgtdb_obj):
 					if(qry_cdr3_start<=qry_cdr3_end):
 						#good
 						cdr3_len=qry_cdr3_end-qry_cdr3_start+1
-						#translation=biopythonTranslate(coding_seq)
-						#print "the coding seq ("+dm+") is : ",coding_seq
-						#print "The translation ("+dm+") is : ",translation
-						#print "CDR3_LEN ("+dm+") ="+str(cdr3_len)
 						cdr3_hist[dm]=cdr3_len
 						cdr3_hist[dm+'_from']=qry_cdr3_start
 						cdr3_hist[dm+'_to']=qry_cdr3_end
@@ -449,10 +445,10 @@ def CDR3LengthAnalysis(vMap,jMap,organism,imgtdb_obj):
 					#print "BADQRYMAP Failure to map to query for mode=",dm," V=",currentV," J=",currentJ," read=",vMap['query id'],"  REFSTART=",ref_cdr3_start,"QRYSTART=",qry_cdr3_start,"REFEND=",ref_cdr3_end,"QRYEND=",qry_cdr3_end
 					pass
 			else:
-				#print "BADREFMAP mode=",dm," refVCDR3=(-1) for ",currentV," = ",ref_cdr3_start," or refJCDR3 ",currentJ," = ",ref_cdr3_end
+				#print "BADREFMAP mode=",dm," VALLELE=",currentV," and JALLELE=",currentJ," refVCDR3=(-1) for ",currentV," = ",ref_cdr3_start," or refJCDR3 ",currentJ," = ",ref_cdr3_end
 				pass
 	else:
-		#print "Ref names ",currentV," and ",currentJ," don't appear alleleic!"
+		print "Ref names ",currentV," and ",currentJ," don't appear alleleic!"
 		pass
 	#print "***************************\n"
 	#print "RETURNING THIS CDR3_HIST for ",vMap['query id'],":"
