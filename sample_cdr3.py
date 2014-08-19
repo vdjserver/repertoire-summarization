bbc3bc13261843411523d08ae9436915c59a9ece
Edward A. Salinas <edward.salinas@utsouthwestern.edu>
2014-08-19 09:57:13 -0500

    sorted CDR3 length table outputted....next is the diver analysis

--- sample_cdr3.py
+++ sample_cdr3.py
@@ -1,24 +1,32 @@
 #!/usr/bin/env python
 
 from utils import extractAsItemOrFirstFromList,canReadAccess
+import fnmatch
 import argparse
 import re
+import os
 
 def getBatchIDAndSampleIDFromPath(tsv_path):
-	return ["BID","SID"]
+	bsre=re.compile(r'/([^/]+)/(Sample[^\.]+)\.fasta')
+	re_res=re.search(bsre,tsv_path)
+	if(re_res):
+		batch=re_res.group(1)
+		sample=re_res.group(2)
+		return [batch,sample]
+	else:
+		return ["BID_ERR","SID_ERR"]
 
 
 def cdr3LenStats(dir_base,out_hist,min_len,max_len):
 	file_num=0
-	for root, dir, files in os.walk(dir_base):
-		for items in fnmatch.filter(files, "*out.tsv"):
+	for root, dirs, files in sorted(os.walk(dir_base)):
+		for items in sorted(fnmatch.filter(files, "*out.tsv")):
 			full_tsv_path=root+"/"+items
 			reader=open(full_tsv_path,'r')
 			line_num=1
 			len_hist_this_file=dict()
 			for line in reader:
 				if(line_num>1):
-					pass
 					pieces=line.split('\t')
 					status=pieces[183]
 					if(status=="OK"):
@@ -31,12 +39,25 @@ def cdr3LenStats(dir_base,out_hist,min_len,max_len):
 								len_hist_this_file[cdr3_na_len]=1
 				line_num+=1
 			reader.close()
-			writer=open(out_hist,'a')
 			if(file_num==0):
 				#print headers
-				pass
+				writer=open(out_hist,'w')
+				writer.write("batch\tsample")
+				for val in range(min_len,max_len+1):
+					writer.write("\tCDR3_LEN_"+str(val))
+				writer.write("\n")
+				writer.close()
+			writer=open(out_hist,'a')
+			bs=getBatchIDAndSampleIDFromPath(full_tsv_path)
+			print "Writing CDR3 len/hist data for",bs
+			writer.write(bs[0]+"\t"+bs[1])
 			for val in range(min_len,max_len+1):
-								
+				if(val in len_hist_this_file):
+					writer.write("\t"+str(len_hist_this_file[val]))
+				else:
+					writer.write("\t"+str(0))
+			writer.write("\n")
+			writer.close()
 			file_num+=1
 
 
@@ -45,6 +66,7 @@ def cdr3LenStats(dir_base,out_hist,min_len,max_len):
 def cdr3DiverStats(dir_base,out_diva):
 	for root, dir, files in os.walk(dir_base):
 		full_tsv_path=root+"/"+items
+
 		reader=open(full_tsv_path,'r')
 		line_num=1
 		cdr3_na_this_file=dict()
@@ -83,9 +105,12 @@ def getCDR3MinMax(dir_base):
 	max_cdr3=None
 	min_cdr3=None
 	cdr3_len_set=set()
-	for root, dir, files in os.walk(dir_base):
+	for root, dirs, files in os.walk(dir_base):
 		for items in fnmatch.filter(files, "*out.tsv"):
 			full_tsv_path=root+"/"+items
+			#print "Analyzing file ",full_tsv_path," for CDR3 min/max length...."
+			bs=getBatchIDAndSampleIDFromPath(full_tsv_path)
+			#print "The batch and sample :",bs
 			reader=open(full_tsv_path,'r')
 			line_num=1
 			for line in reader:
@@ -111,24 +136,27 @@ def getCDR3MinMax(dir_base):
 
 
 if (__name__=="__main__"):
-	parser.add_argument('tsv_in',type=str,nargs=1,help="path to a rep_char TSV output file")
-	parser.add_argument('bid',type=str,nargs=1,help="string for BATCH ID")
-	parser.add_argument('sid',type=str,nargs=1,help="string for SAMPLE ID")
+	parser=argparse.ArgumentParser(description='Print DIOGENIX formatted rep_char CDR3 statistics')
 	parser.add_argument('out_hist',type=str,nargs=1,help="output file path for TSV of sample/CDR3 length table")
 	parser.add_argument('out_diva',type=str,nargs=1,help="output file path for TSV of CDR3 length diversity analysis")
 	parser.add_argument('dir_base',type=str,nargs=1,help="base dir for CDR3 length hist")
 	args=parser.parse_args()
 	if(not(args)):
 		parser.print_help()
-	input_file_path=extractAsItemOrFirstFromList(args.tsv_in)
-	if(not(canReadAccess(input_file_path))):
-		parser.print_help()
-	bid=extractAsItemOrFirstFromList(args.bid)
-	sid=extractAsItemOrFirstFromList(args.sid)
-	out_hist=extractAsItemOrFirstFromList(args.out_hist)
-	out_diva=extractAsItemOrFirstFromList(args.out_diva)
-	
-
+	else:
+		out_hist=extractAsItemOrFirstFromList(args.out_hist)
+		out_diva=extractAsItemOrFirstFromList(args.out_diva)
+		dir_base=extractAsItemOrFirstFromList(args.dir_base)
+		if(not(os.path.exists(dir_base))):
+			parser.print_help()
+		else:
+			print "using dir_base ",dir_base
+			cdr3_min_max=getCDR3MinMax(dir_base)
+			min_len=cdr3_min_max[0]
+			max_len=cdr3_min_max[1]
+			print "min/max are :",cdr3_min_max
+			print "Writing histogram to",out_hist
+			cdr3LenStats(dir_base,out_hist,min_len,max_len)
 
 
 
