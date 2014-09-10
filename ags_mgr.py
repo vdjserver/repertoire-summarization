4731980ec661f69d3aea0c96f1512ba1e8e7daae
Edward A. Salinas <edward.salinas@utsouthwestern.edu>
2014-09-09 19:00:38 -0500

    de-couple from codon_analysis

--- ags_mgr.py
+++ ags_mgr.py
@@ -4,47 +4,67 @@ import fileinput
 from codon_analysis import codonCounter
 
 class ags_manager:
-
-	codonCounterObj=None
+	ags6_count=0
+	ags5_count=0
+	rm_count=0
 	ags6_nums=None
 	ags5_nums=None
 	name=None
-
 	def __init__(self,init_name):
 		self.name=init_name
-		self.codonCounterObj=codonCounter(init_name)
-		self.ags6_nums=self.codonCounterObj.get_ags6_nums()
-		self.ags5_nums=self.codonCounterObj.get_ags5_nums()
-		
-		
-
+		#self.codonCounterObj=codonCounter(init_name)
+		self.ags6_nums=["31B","40","56","57","81","89"]
+		self.ags5_nums=["31B","40","56","57","81"]
+	
 	def receive_numbered_mut(self,numbered_mut):
 		mut_pieces=self.appearsToBeNumberedMut(numbered_mut)
 		if(mut_pieces!=None):
 			numbered_pos=mut_pieces[1]
 			if(numbered_pos in self.ags6_nums):
-				self.codonCounterObj.increment_ags6_rep_muts(numbered_pos)
+				self.ags6_count+=1
 			if(numbered_pos in self.ags5_nums):
-				self.codonCounterObj.increment_ags5_rep_muts(numbered_pos)			
-			self.codonCounterObj.sampleRepMuts.increment(numbered_pos)
-
+				self.ags5_count+=1
+			self.rm_count+=1
 	
 	def appearsToBeNumberedMut(self,nm):
-		return self.codonCounterObj.appearsToBeNumberedMut(nm)
-
-
+		#return self.codonCounterObj.appearsToBeNumberedMut(nm)
+		nmRe=re.compile(r'^([A-Z\*])([0-9][0-9][ABC]?)([A-Z\*])$')
+		nmmo=re.search(nmRe,nm)
+		if(nmmo):
+			mut_from=nmmo.group(1)
+			mut_pos=nmmo.group(2)
+			mut_to=nmmo.group(3)
+			return [str(mut_from),str(mut_pos),str(mut_to)]
+		else:
+			return False
+	
+	
+	
 	def compute_ags(self,mode="AGS6"):
 		#print "mgr named ",self.name,"giving ags from counter obj named ",self.codonCounterObj.name
 		if(mode=="AGS6"):
-			ags6_score=self.codonCounterObj.computeAGS()
-			sampAGSTot=float(self.codonCounterObj.computeAGS6TotRM())
-			sampTot=float(self.codonCounterObj.computeSampTotRM())
+			sampTot=self.rm_count
+			if(sampTot<=0):
+				#avoid division by zero error
+				return None			
+			sampAGSTot=float(self.ags6_count)
+			pct_ags6=(sampAGSTot/sampTot)*100.0
+			ags_numerator=pct_ags6-1.6*6.0
+			ags_denominator=0.9
+			ags6_score=ags_numerator/ags_denominator
 			return [ags6_score,sampAGSTot,sampTot]
 		elif(mode=="AGS5"):
-			ags5_score=self.codonCounterObj.computeAGS5()
-			sampAGSTot=float(self.codonCounterObj.computeAGS5TotRM())
-			sampTot=float(self.codonCounterObj.computeSampTotRM())
+			sampTot=self.rm_count
+			if(sampTot<=0):
+				#avoid division by zero error
+				return None	
+			sampAGSTot=float(self.ags5_count)
+			pct_ags5=(sampAGSTot/sampTot)*100.0
+			ags_numerator=pct_ags5-1.6*5.0
+			ags_denominator=0.9
+			ags5_score=ags_numerator/ags_denominator
 			return [ags5_score,sampAGSTot,sampTot]
+
 		else:
 			raise Exception("ERROR, UNHANDLED AGS MODE = "+mode)
 
