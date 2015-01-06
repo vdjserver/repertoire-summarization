0fb92fd6c5295e3551b714ad2da010042dc26d3a
Edward A. Salinas <edward.salinas@utsouthwestern.edu>
2015-01-06 14:00:45 -0600

    rename column Sequence similarity to V Sequence similarity

--- rep_char.py
+++ rep_char.py
@@ -344,7 +344,7 @@ def returnVAlnAllCharMap(vInfo,imgtdb_obj,read_result_obj=None,meta=None):
 			newKey=newKey[0].upper()+newKey[1:]
 			new_map[newKey+ " (over V)"]=vCharMap[k]
 			if(k=="homology%"):
-				new_map['Sequence similarity']=vCharMap[k]
+				new_map['V Sequence similarity']=vCharMap[k]
 		new_map['V length (nucleotides)']=abs(vInfo['q. start']-vInfo['q. end'])
 		return new_map
 
@@ -792,7 +792,7 @@ def appendAnnToFileWithMap(fHandl,m,rid,read_name,desiredKeys=None,defaultValue=
 	"V gene",
 	"J gene",
 	"D gene",
-	"Sequence similarity",
+	"V Sequence similarity",
 
 	#PART 2 : post-alignment filter reports for standard use
 	"Out-of-frame junction",
