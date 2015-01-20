6a9002c05b34754682cd5e88aae3514c6faf26d1
Edward A. Salinas <edward.salinas@utsouthwestern.edu>
2015-01-20 14:59:39 -0600

    add more stop comments

--- rep_char.py
+++ rep_char.py
@@ -215,21 +215,22 @@ def getIndelsFlags(vInfo,dInfo,jInfo):
 
 
 
-
-
-
-
-
 def getValnWholeSeqStopFlag(vInfo,dInfo,jInfo,imgtdb_obj,organism,annMap,seq_rec):
 	if(vInfo==None):
 		#if no V alignment how can there be a frame, a J alignment, or a stop codon?
 		return None
 	else:
 		vAlnObj=alignment(vInfo['query seq'],vInfo['subject seq'],vInfo['q. start'],vInfo['q. end'],vInfo['s. start'],vInfo['s. end'])
-		#print "Looking at STOP log for ",str(seq_rec.id)
-		#print "The alignment plain is \n",vAlnObj.getNiceString()
+		print "Looking at STOP log for ",str(seq_rec.id)
+		print "V allele is ",vInfo['subject ids']
+		if(jInfo!=None and 'subject ids' in jInfo):
+			print "J allele is ",jInfo['subject ids']
+		if(dInfo!=None and 'subject ids' in dInfo):
+			print "D allele is ",dInfo['subject ids']
+			
+		print "The V alignment plain is \n",vAlnObj.getNiceString()
 		vAlnNoGapStart=vAlnObj.getGEQAlignmentFirstNonGap()
-		#print "The alignment non-gap start is ",vAlnNoGapStart.getNiceString()
+		print "The V alignment non-gap ('de-gapped' begin') start is ",vAlnNoGapStart.getNiceString()
 		s_start=int(vInfo['s. start'])
 		q_start=int(vInfo['q. start'])
 		s_start_frame=getTheFrameForThisReferenceAtThisPosition(vInfo['subject ids'],organism,imgtdb_obj,s_start)
@@ -243,7 +244,7 @@ def getValnWholeSeqStopFlag(vInfo,dInfo,jInfo,imgtdb_obj,organism,annMap,seq_rec
 		else:
 			info_to_use=jInfo
 		#use max in case there is germline alignment overlap
-		#print "S start frame is ",s_start_frame
+		print "S start frame is ",s_start_frame
 		q_end=max(int(info_to_use['q. end']),int(vInfo['q. end']))
 		if(not(vInfo['is_inverted'])):
 			#print "Computing to_trans regular block...."
@@ -252,22 +253,22 @@ def getValnWholeSeqStopFlag(vInfo,dInfo,jInfo,imgtdb_obj,organism,annMap,seq_rec
 			proper=rev_comp_dna(str(seq_rec.seq))
 			to_trans=str(proper[q_start-1:q_end]).upper()
 			#print "computing to_trans inverted block"
-		#print "s_start_frame is ",s_start_frame
+		print "s_start_frame is ",s_start_frame
 		if(s_start_frame!=0):
 			to_trans=to_trans[(3-s_start_frame):]
-		#print "To be translated for (pre-trim) read=",str(seq_rec.id)," is ",to_trans
+		print "To be translated for (adjusted for start frame) (pre-trim by alignment) read=",str(seq_rec.id)," is\n",to_trans
 		to_trans_len=len(to_trans)
 		if((to_trans_len%3)!=0):
 			necessary_trim=(to_trans_len%3)
 			trim_to_trans=to_trans[:to_trans_len-necessary_trim]
 		else:
 			trim_to_trans=to_trans
-		#print "The trim_to_trans is ",trim_to_trans
+		print "The trim_to_trans is (trimmed by V alignment and the more extensive of D or J) is\n",trim_to_trans
 		translation=codonAnalyzer.fastTransStr(trim_to_trans)
 		stopFlag=stringContainsAStar(translation)
-		#print "The translation : ",translation
-		#print "The stop flag : ",stopFlag
-		#print "\n\n\n\n"
+		print "The translation : ",translation
+		print "The stop flag : ",stopFlag
+		print "\n\n\n\n"
 		return stopFlag
 
 		
