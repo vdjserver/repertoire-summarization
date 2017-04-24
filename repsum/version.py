39d678aee54976355707cb6307d144f67969caef
Scott Christley <Scott.Christley@UTSouthwestern.edu>
2017-04-24 13:32:56 -0500

    new version

--- repsum/version.py
+++ repsum/version.py
@@ -3,7 +3,7 @@ Version and authorship information
 """
 __author__    = 'VDJServer Team'
 __email__     = 'vdjserver@utsouthwestern.edu'
-__copyright__ = 'Copyright (C) 2014-2016 The University of Texas Southwestern Medical Center'
+__copyright__ = 'Copyright (C) 2014-2017 The University of Texas Southwestern Medical Center'
 __license__   = 'MIT'
-__version__   = '1.0.0'
-__date__      = '2016.07.28'
+__version__   = '1.0.1'
+__date__      = '2017.04.24'
