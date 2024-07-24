1cf8fb14b34416084d3cef1308847eaab27d5dd5
Scott Christley <scott.christley@utsouthwestern.edu>
2024-07-24 13:24:22 -0500

    update to newest versioneer

--- repcalc/__init__.py
+++ repcalc/__init__.py
@@ -4,6 +4,5 @@ Repertoire-summarization
 
 __copyright__ = 'Copyright (C) 2014-2023 The University of Texas Southwestern Medical Center'
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
