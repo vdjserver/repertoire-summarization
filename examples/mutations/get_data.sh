8a570300676ddfcc2942cef266dd4ff1c3911c85
Sam Wollenburg <Samuel.Wollenburg@UTSouthwestern.edu>
2025-10-27 13:05:40 -0500

    comment out commands to be run manually

--- examples/mutations/get_data.sh
+++ examples/mutations/get_data.sh
@@ -7,8 +7,8 @@
 
 # You will need to manually run these commands
 
-vdjserver-tools files download /projects/c0aef9ef-362d-46fd-abba-39ac0945bd66/analyses/d9c76906-f138-49d7-814f-490f3a7f7124-007/d9c76906-f138-49d7-814f-490f3a7f7124-007.zip
-vdjserver-tools files download /projects/c0aef9ef-362d-46fd-abba-39ac0945bd66/files/repertoires.airr.json
+# vdjserver-tools files download /projects/c0aef9ef-362d-46fd-abba-39ac0945bd66/analyses/d9c76906-f138-49d7-814f-490f3a7f7124-007/d9c76906-f138-49d7-814f-490f3a7f7124-007.zip
+# vdjserver-tools files download /projects/c0aef9ef-362d-46fd-abba-39ac0945bd66/files/repertoires.airr.json
 
 unzip d9c76906-f138-49d7-814f-490f3a7f7124-007.zip
 
